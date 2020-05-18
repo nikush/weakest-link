f5886b779ce2e87505c5f703ae3119c697fae757
Nikush Patel <nikush.patel7@gmail.com>
2020-05-18 07:11:20 +0100

    Show keyboard shortcuts under help

--- js/components/Controls.js
+++ js/components/Controls.js
@@ -5,18 +5,34 @@ Vue.component('Controls', {
                 <button class="btn btn-outline-primary btn-block" @click="toggleGameState">
                     {{gameStateCopy}} Round
                 </button>
-                <button class="btn btn-outline-secondary btn-block" @click="toggleSounds">
-                    {{sharedState.muted?'Unmute':'Mute'}} Sounds
-                </button>
                 <button class="btn btn-outline-secondary btn-block" disabled>
                     Undo Last Action
                 </button>
+                <hr class="border-secondary"/>
+                <button class="btn btn-outline-secondary btn-block" @click="toggleSounds">
+                    {{sharedState.muted?'Unmute':'Mute'}} Sounds
+                </button>
+                <button class="btn btn-outline-secondary btn-block" @click="showHelp=!showHelp">
+                    Help
+                </button>
+                <ul class="list-unstyled mb-0 mt-3" v-if="showHelp">
+                    <li><kbd>s</kbd> start the round</li>
+                    <li><kbd>space</kbd> correct answer, step chain forward</li>
+                    <li><kbd>backspace</kbd> incorrect answer, break chain</li>
+                    <li><kbd>enter</kbd> bank</li>
+                    <li><kbd>up/down</kbd> amend place in chain up/down one step</li>
+                    <!--
+                    <li><kbd>left/right</kbd> amend value in the bank by increments of 2</li>
+                    <li><kbd>z</kbd> undo the last bank/chain break</li>
+                    -->
+                </ul>
             </div>
         </div>
     `,
     data: function () {
         return {
             sharedState: Game.state,
+            showHelp: false,
         }
     },
     methods: {
